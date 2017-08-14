a585af0d9373d8eaad0e11de3769b473b9589fda
Jose Moreno <morenojr90@yahoo.com>
2017-08-14 12:32:29 -0700

    Finish weekdayfunction

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,10 +1,33 @@
 module.exports = {
   weekday: function(date) {
     var numOfDay = date.getDay();
-    console.log(numOfDay)
+    switch(numOfDay) {
+      case 0:
+              numOfDay = "Sunday";
+              break;
+      case 1:
+              numOfDay = "Monday";
+              break;
+      case 2:
+              numOfDay = "Tuesday";
+              break;
+      case 3:
+              numOfDay = "Wednesday";
+              break;
+      case 4:
+              numOfDay = "Thursday";
+              break;
+      case 5:
+              numOfDay = "Friday";
+              break;
+      case 6:
+              numOfDay = "Saturday";
+              break;
+    }
+    return numOfDay;
 
   }
 }
 
 let dateA = new Date(2017, 7, 14)
-module.exports.weekday(dateA);
+console.log(module.exports.weekday(dateA));
