6aab5be9d9e452d85feede8cb70761b2168ece8c
Jose Moreno <morenojr90@yahoo.com>
2017-08-16 10:12:58 -0700

    Final correction, update comments

--- part-2/searchByModel.js
+++ part-2/searchByModel.js
@@ -1,12 +1,16 @@
-const cars = require('./cars.json')
-const model = process.argv[2];
+//searchByModel.js takes one argument (let's call it the model), finds all the cars from cars.json
+// who have a model value that exactly matches the model argument, and prints the id, model, year,
+//last_owner, and date_purchased of each matching car.
+
+const cars = require('./cars.json')  //requires json file
+const model = process.argv[2];      //saves argument passed in to model variable
 
 for (var index in cars) {
-  var cityName = cars[index].model
-  if(cityName.toUpperCase() == model.toUpperCase())
-    console.log("ID: " + cars[index].id + ", Model: " + cars[index].model +
+  var cityName = cars[index].model  //cityName will hold the current objects model value
+  if(cityName.toUpperCase() == model.toUpperCase())  //makes sure it matches exactly
+    console.log("ID: " + cars[index].id + ", Model: " + cars[index].model +  //logs out car info
                 ", Year: " + cars[index].year + ", Last Owner: " + cars[index].last_owner +
-                 ", Date Purchased:" + cars[index].date_purchased );
+                ", Date Purchased:" + cars[index].date_purchased );
 
 
 }
